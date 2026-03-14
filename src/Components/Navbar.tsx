3f65f62f252d692a70a948cabe70a74ebd902827
ahketabdar <kh.amir1024@gmail.com>
2026-03-14 07:37:54 -0700

    add scripts for vercel build'git commit -m

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 function Navbar() {
 
   const [spin, setSpin] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function handleClick() {
     setSpin(true);
@@ -14,30 +15,52 @@ function Navbar() {
     }, 600);
   }
 
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <nav className={styles.navbar}>
 
-      <Link 
-        to="/" 
-        className={`${styles.logo} ${spin ? styles.rotate : ""}`} 
+      <Link
+        to="/"
+        className={`${styles.logo} ${spin ? styles.rotate : ""}`}
         onClick={handleClick}
       >
         PhoenixCores
       </Link>
 
-      <ul className={styles.menu}>
-        <li><Link to="/">Home</Link></li>
+      {/* Hamburger icon */}
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <ul className={`${styles.menu} ${menuOpen ? styles.active : ""}`}>
 
         <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li className={styles.hasDropdown}>
           About
           <ul className={styles.dropdown}>
-            <li><Link to="/About">About me</Link></li>
-            <li><Link to="/Resume">Download Resume</Link></li>
+            <li>
+              <Link to="/About">About me</Link>
+            </li>
+            <li>
+              <Link to="/Resume">Download Resume</Link>
+            </li>
           </ul>
         </li>
 
-        <li><Link to="/Articles">Articles</Link></li>
-        <li><Link to="/Contact">Contacts</Link></li>
+        <li>
+          <Link to="/Articles">Articles</Link>
+        </li>
+
+        <li>
+          <Link to="/Contact">Contacts</Link>
+        </li>
+
       </ul>
 
     </nav>
