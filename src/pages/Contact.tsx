58f9334d0f2229a3279980f97666b29ed0b6b635
ahketabdar <kh.amir1024@gmail.com>
2026-06-10 06:59:41 -0700

    Update project

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,28 +1,61 @@
 import styles from './Contact.module.css';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-    return(
-        <form className={styles.contactForm}>
-            <div>
-                <label>Your Name:</label>
-                <input type="text" placeholder="Enter Your Name Here..." />
-            </div>
+  const form = useRef<HTMLFormElement>(null);
 
-            <div>
-                <label>Your Phone Number or Email:</label>
-                <input type="text" placeholder="Enter Your Contact Info" />
-            </div>
+ const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
 
-            <div>
-                <label>Project Description:</label>
-                <textarea placeholder="Your Website Description"></textarea>
-            </div>
+  const formData = new FormData(e.currentTarget);
 
-            <div>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
-    )
+  emailjs.send(
+    'service_07x17fi',
+    'template_jkdc1ko',
+    {
+      user_name: formData.get('user_name'),
+      user_contact: formData.get('user_contact'),
+      message: formData.get('message'),
+    },
+    'YOUR_PUBLIC_KEY'
+  )
+  .then((res) => {
+    console.log("SUCCESS:", res.status);
+    alert("Message Sent!");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err);
+  });
+
+  e.currentTarget.reset();
+};
+  
+
+  return (
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className={styles.contactForm}
+    >
+      <div>
+        <label>Your Name:</label>
+        <input name="user_name" type="text" required />
+      </div>
+
+      <div>
+        <label>Your Phone Number or Email:</label>
+        <input name="user_contact" type="text" required />
+      </div>
+
+      <div>
+        <label>Project Description:</label>
+        <textarea name="message" required></textarea>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Contact;
